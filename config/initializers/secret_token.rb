3be9cec365b342371c0b5478e47c61a0d5180979
jimmy kung <jimmy_kung@hotmail.com>
2011-08-25 02:49:38 +0000

    Commit of rails environment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apbnewsletter::Application.config.secret_token = '0cbaf93760cd782e892ec75edbdf1f636c001577a8f998ea39e17ec7223098f4c14cd774c08586ac274fffabcc7fe79b6cf5ce85d45c88a9ab11e9b0745655ea'
